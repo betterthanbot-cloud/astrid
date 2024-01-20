b622a0feefd88b05149f2acde3ab09eecc127b38
betterthanbot <gibsonorx@gmail.com>
2024-01-20 22:11:00 +0800

    refactor

--- terraform/products/02-eks/cluster.tf
+++ terraform/products/02-eks/cluster.tf
@@ -13,18 +13,33 @@ module "eks" {
 
   cluster_addons = {
     coredns = {
-      most_recent = true
+      most_recent                 = true
+      resolve_conflicts_on_create = "OVERWRITE"
+      configuration_values = jsonencode({
+        replicaCount = 4
+        resources = {
+          limits = {
+            cpu    = "100m"
+            memory = "150Mi"
+          }
+          requests = {
+            cpu    = "50m"
+            memory = "150Mi"
+          }
+        }
+      })
     }
+
     kube-proxy = {
       most_recent = true
     }
+
     vpc-cni = {
       most_recent              = true
       before_compute           = true
       service_account_role_arn = module.vpc_cni_irsa.iam_role_arn
       configuration_values = jsonencode({
         env = {
-          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
           ENABLE_PREFIX_DELEGATION = "true"
           WARM_PREFIX_TARGET       = "1"
         }
@@ -39,39 +54,24 @@ module "eks" {
   manage_aws_auth_configmap = true
 
   eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["t3.medium"]
-
-    # We are using the IRSA created below for permissions
-    # However, we have to deploy with the policy attached FIRST (when creating a fresh cluster)
-    # and then turn this off after the cluster/node group is created. Without this initial policy,
-    # the VPC CNI fails to assign IPs and nodes cannot join the cluster
-    # See https://github.com/aws/containers-roadmap/issues/1666 for more context
+    ami_type                   = "AL2_x86_64"
+    instance_types             = ["t3.medium"]
     iam_role_attach_cni_policy = true
   }
 
   eks_managed_node_groups = {
-    # Default node group - as provided by AWS EKS
     default_node_group = {
-      # By default, the module creates a launch template to ensure tags are propagated to instances, etc.,
-      # so we need to disable it to use the default template provided by the AWS EKS managed node group service
       use_custom_launch_template = false
-
-      disk_size = 50
-
-      # Remote access cannot be specified with a launch template
+      disk_size                  = 50
       remote_access = {
         ec2_ssh_key               = module.key_pair.key_pair_name
         source_security_group_ids = [aws_security_group.remote_access.id]
       }
     }
-
-    # Complete
     complete = {
       name            = "complete-eks-mng"
       use_name_prefix = true
-
-      subnet_ids = module.vpc.private_subnets
+      subnet_ids      = module.vpc.private_subnets
 
       min_size     = 1
       max_size     = 3
@@ -148,27 +148,6 @@ module "eks" {
         additional                         = aws_iam_policy.node_additional.arn
       }
 
-      # schedules = {
-      #   scale-up = {
-      #     min_size     = 2
-      #     max_size     = "-1" # Retains current max size
-      #     desired_size = 2
-      #     start_time   = "2023-03-05T00:00:00Z"
-      #     end_time     = "2024-03-05T00:00:00Z"
-      #     time_zone    = "Etc/GMT+0"
-      #     recurrence   = "0 0 * * *"
-      #   },
-      #   scale-down = {
-      #     min_size     = 0
-      #     max_size     = "-1" # Retains current max size
-      #     desired_size = 0
-      #     start_time   = "2023-03-05T12:00:00Z"
-      #     end_time     = "2024-03-05T12:00:00Z"
-      #     time_zone    = "Etc/GMT+0"
-      #     recurrence   = "0 12 * * *"
-      #   }
-      # }
-
       tags = {
         ExtraTag = "EKS managed node group complete example"
       }
@@ -182,82 +161,6 @@ module "eks" {
 # Supporting Resources
 ################################################################################
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
-
-  name = local.name
-  cidr = local.vpc_cidr
-
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
-  intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
-
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  enable_ipv6            = true
-  create_egress_only_igw = true
-
-  public_subnet_ipv6_prefixes                    = [0, 1, 2]
-  public_subnet_assign_ipv6_address_on_creation  = true
-  private_subnet_ipv6_prefixes                   = [3, 4, 5]
-  private_subnet_assign_ipv6_address_on_creation = true
-  intra_subnet_ipv6_prefixes                     = [6, 7, 8]
-  intra_subnet_assign_ipv6_address_on_creation   = true
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
-
-  tags = local.tags
-}
-
-module "vpc_cni_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.0"
-
-  role_name_prefix      = "VPC-CNI-IRSA"
-  attach_vpc_cni_policy = true
-  vpc_cni_enable_ipv6   = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-node"]
-    }
-  }
-
-  tags = local.tags
-}
-
-module "ebs_kms_key" {
-  source  = "terraform-aws-modules/kms/aws"
-  version = "~> 1.5"
-
-  description = "Customer managed key to encrypt EKS managed node group volumes"
-
-  # Policy
-  key_administrators = [
-    data.aws_caller_identity.current.arn
-  ]
-
-  key_service_roles_for_autoscaling = [
-    # required for the ASG to manage encrypted volumes for nodes
-    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
-    # required for the cluster / persistentvolume-controller to create encrypted PVCs
-    module.eks.cluster_iam_role_arn,
-  ]
-
-  # Aliases
-  aliases = ["eks/${local.name}/ebs"]
-
-  tags = local.tags
-}
 
 module "key_pair" {
   source  = "terraform-aws-modules/key-pair/aws"
@@ -268,51 +171,6 @@ module "key_pair" {
 
   tags = local.tags
 }
-
-resource "aws_security_group" "remote_access" {
-  name_prefix = "${local.name}-remote-access"
-  description = "Allow remote SSH access"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    description = "SSH access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = merge(local.tags, { Name = "${local.name}-remote" })
-}
-
-resource "aws_iam_policy" "node_additional" {
-  name        = "${local.name}-additional"
-  description = "Example usage of node additional policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-
-  tags = local.tags
-}
-
 data "aws_ami" "eks_default" {
   most_recent = true
   owners      = ["amazon"]
@@ -332,62 +190,3 @@ data "aws_ami" "eks_default_arm" {
     values = ["amazon-eks-arm64-node-${local.cluster_version}-v*"]
   }
 }
-
-data "aws_ami" "eks_default_bottlerocket" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["bottlerocket-aws-k8s-${local.cluster_version}-x86_64-*"]
-  }
-}
-
-################################################################################
-# Tags for the ASG to support cluster-autoscaler scale up from 0
-################################################################################
-
-# locals {
-
-#   # We need to lookup K8s taint effect from the AWS API value
-#   taint_effects = {
-#     NO_SCHEDULE        = "NoSchedule"
-#     NO_EXECUTE         = "NoExecute"
-#     PREFER_NO_SCHEDULE = "PreferNoSchedule"
-#   }
-
-#   cluster_autoscaler_label_tags = merge([
-#     for name, group in module.eks.eks_managed_node_groups : {
-#       for label_name, label_value in coalesce(group.node_group_labels, {}) : "${name}|label|${label_name}" => {
-#         autoscaling_group = group.node_group_autoscaling_group_names[0],
-#         key               = "k8s.io/cluster-autoscaler/node-template/label/${label_name}",
-#         value             = label_value,
-#       }
-#     }
-#   ]...)
-
-#   cluster_autoscaler_taint_tags = merge([
-#     for name, group in module.eks.eks_managed_node_groups : {
-#       for taint in coalesce(group.node_group_taints, []) : "${name}|taint|${taint.key}" => {
-#         autoscaling_group = group.node_group_autoscaling_group_names[0],
-#         key               = "k8s.io/cluster-autoscaler/node-template/taint/${taint.key}"
-#         value             = "${taint.value}:${local.taint_effects[taint.effect]}"
-#       }
-#     }
-#   ]...)
-
-#   cluster_autoscaler_asg_tags = merge(local.cluster_autoscaler_label_tags, local.cluster_autoscaler_taint_tags)
-# }
-
-# resource "aws_autoscaling_group_tag" "cluster_autoscaler_label_tags" {
-#   for_each = local.cluster_autoscaler_asg_tags
-
-#   autoscaling_group_name = each.value.autoscaling_group
-
-#   tag {
-#     key   = each.value.key
-#     value = each.value.value
-
-#     propagate_at_launch = false
-#   }
-# }
